fcbafa24a484a94069f25abceb5de5623353293e
Kgomotso <khomcy.sito@gmail.com>
2023-04-27 23:26:48 +0200

    Added JWT authentication and authorization to API

---
+++ BankAPI.Tests/Controllers/LoginControllerTests.cs
@@ -0,0 +1,45 @@
+﻿
+using System.IdentityModel.Tokens.Jwt;
+using BankAPI.Controllers;
+using BankAPI.Models;
+using BankAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using Moq;
+using Xunit;
+
+namespace BankAPI.Tests.Controllers;
+public class LoginControllerTests
+{
+	private readonly Mock<IUserService> _userService;
+	private readonly LoginController _loginController;
+
+    public LoginControllerTests()
+    {
+        _userService = new Mock<IUserService>();
+        _loginController = new LoginController(_userService.Object);
+    }
+
+    [Fact]
+    public void Authenticate_ValidCredentials_ReturnsOkObjectResult()
+    {
+        var user = new User { Username = "bankUser", Password = "bankPassword" };
+        _userService.Setup(x => x.Authenticate(user.Username, user.Password)).Returns(true);
+
+        var result = _loginController.Authenticate(user);
+
+        Assert.IsType<OkObjectResult>(result);
+    }
+
+    [Fact]
+    public void Authenticate_InvalidCredentials_ReturnsUnauthorizedResult()
+    {
+        var user = new User { Username = "invalidUser", Password = "invalidPassword" };
+        _userService.Setup(x => x.Authenticate(user.Username, user.Password)).Returns(false);
+
+        var result = _loginController.Authenticate(user);
+
+        Assert.IsType<UnauthorizedResult>(result);
+    }
+}
+
---
+++ Bank_API/Controllers/LoginController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BankAPI.Services;
+using BankAPI.Models;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.AspNetCore.Authorization;
+using System;
+
+namespace BankAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public LoginController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost("login")]
+        [AllowAnonymous]
+        public IActionResult Authenticate(User user)
+        {
+            var authenticatedUser = _userService.Authenticate(user.Username, user.Password);
+
+            if (!authenticatedUser)
+            {
+                return Unauthorized();
+            }
+
+            var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("veryHardToGuess@SecreteKey"));
+            var signinCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
+            var tokenOptions = new JwtSecurityToken(
+                issuer: "BankAPI",
+                audience: "https://localhost:7174",
+                claims: new List<Claim>(),
+                expires: DateTime.Now.AddMinutes(5),
+                signingCredentials: signinCredentials
+            );
+
+            var tokenString = new JwtSecurityTokenHandler().WriteToken(tokenOptions);
+            return Ok(new { Token = tokenString });
+        }
+    }
+}
